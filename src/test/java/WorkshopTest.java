e066e6133649a16735613bd11ceb4e14dc1e9948
SneiderGL <hsgrajalesl@udistrital.edu.co>
2025-12-04 23:23:27 -0500

    se acaba de Implementar invertirCadena

--- src/test/java/WorkshopTest.java
+++ src/test/java/WorkshopTest.java
@@ -232,7 +232,7 @@ public class WorkshopTest {
         // Test 2: Invertir cadena vacía
         assertEquals("", tester.invertirCadena(""));
         // Test 3: Invertir cadena con caracteres especiales
-        assertEquals("!@#321", tester.invertirCadena("123@#!"));
+        assertEquals("!#@321", tester.invertirCadena("123@#!"));
     }
 
     // Test para el método esPalindromo
