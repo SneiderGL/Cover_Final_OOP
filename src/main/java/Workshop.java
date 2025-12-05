619110492f31d467f43563988f96cc7ab34a40bd
SneiderGL <hsgrajalesl@udistrital.edu.co>
2025-12-04 22:50:43 -0500

    se acaba de Implementar sumaElementos

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -56,9 +56,14 @@ public class Workshop {
 
     // Método que suma todos los elementos de un arreglo
     public int sumaElementos(int[] arreglo) {
-        // TODO: Implementar el método para sumar todos los elementos de un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 15.
-        return 0;
+
+        if (arreglo == null || arreglo.length == 0) {
+            return 0;
+        } int suma = 0;
+        for (int elemento : arreglo) {
+            suma += elemento;
+        }
+        return suma;
     }
 
     // Método que calcula el promedio de los elementos de un arreglo
