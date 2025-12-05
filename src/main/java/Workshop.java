085dcf06c7012a3e3bd6803575eac70147bb4b89
SneiderGL <hsgrajalesl@udistrital.edu.co>
2025-12-04 23:02:41 -0500

    se acaba de Implementar buscarElemento

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -111,9 +111,16 @@ public class Workshop {
 
     // Método que busca un elemento en un arreglo
     public boolean buscarElemento(int[] arreglo, int elemento) {
-        // TODO: Implementar el método para buscar un elemento en un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5] y elemento = 3, el resultado debería ser true.
-        return false;
+       if (arreglo == null) {
+            return false;
+        }
+        for (int actual : arreglo) {
+            if (actual == elemento) {
+                return true; // Encontrado
+            }
+        }
+
+        return false; // No encontrado después de revisar todo el arreglo
     }
 
     // Método que invierte un arreglo
